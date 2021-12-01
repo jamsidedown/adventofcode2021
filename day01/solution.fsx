61bc8ec93dbb866994ef53f4d575ed87b971c61c
jamsidedown <46314954+jamsidedown@users.noreply.github.com>
2021-12-01 07:22:15 +0000

    Added solution for day 1, part 1

--- day01/solution.fsx
+++ day01/solution.fsx
@@ -1,5 +1,20 @@
-let test = "Hello, world!"
-printfn "%s" test
+open System.IO
 
-assert (1 = 2)
-printfn "end"
+let testInput = [| 199; 200; 208; 210; 200; 207; 240; 269; 260; 263; |]
+
+let readInput filepath =
+    File.ReadAllLines filepath
+    |> Array.map int
+
+let partOne (depths:array<int>) =
+    Array.zip depths[1..] depths[..depths.Length-2]
+    |> Array.sumBy (fun (u, l) ->
+        match u, l with
+        | u, l when u > l -> 1
+        | _ -> 0)
+
+assert (partOne testInput = 7)
+
+let input = readInput "day01/input.txt"
+
+printfn $"{partOne input}"
